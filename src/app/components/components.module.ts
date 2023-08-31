55bf1d7faa122072cf221291ec6d005d2b3042b6
Rusu Dinu <dinu@codingshadows.com>
2023-08-31 18:54:31 +0300

    fixed components error

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { DemoComponentComponent } from './demo-component/demo-component.component';
 import { EnvironmentDemoComponentComponent } from './environment-demo-component/environment-demo-component.component';
 import { DatabindingDemoComponentComponent } from './databinding-demo-component/databinding-demo-component.component';
-import { PropertyBindingComponentComponent } from './property-binding-component/property-binding-component.component';
 import { PropertybindingDemoComponentComponent } from './propertybinding-demo-component/propertybinding-demo-component.component';
 import { EventbindingDemoComponentComponent } from './eventbinding-demo-component/eventbinding-demo-component.component';
 import { TwowaybindingDemoComponentComponent } from './twowaybinding-demo-component/twowaybinding-demo-component.component';
@@ -17,7 +16,6 @@ import { PipesDemoComponentComponent } from './pipes-demo-component/pipes-demo-c
     DemoComponentComponent,
     EnvironmentDemoComponentComponent,
     DatabindingDemoComponentComponent,
-    PropertyBindingComponentComponent,
     PropertybindingDemoComponentComponent,
     EventbindingDemoComponentComponent,
     TwowaybindingDemoComponentComponent,
